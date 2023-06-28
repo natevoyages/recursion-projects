706e2f72a66e64f2c04eeb4fc004d0b40d9bdf9c
NateVoyages <allanromo20@gmail.com>
2023-06-28 15:35:27 -0700

    Complete recursive fib (rough)

--- fibonacci-sequence/javascript.js
+++ fibonacci-sequence/javascript.js
@@ -1,4 +1,4 @@
-function fib(num){
+function fibs(num){
     let array = [];
     let num1 = 0;
     let num2 = 0;
@@ -19,8 +19,26 @@ function fib(num){
     return array;
 }
 
-function fibsRec(num){
-    
+function fibsRec(num, i = num, num1 = 0, num2 = 1){
+    i = i || 0;
+    if(i == num){
+        return [num1].concat(fibsRec(num, i - 1));
+    }
+    else if(i == num -1){
+        return [num2].concat(fibsRec(num, i - 1, num2, num2))
+    }
+    else if(i ==  num -2){
+        return [num2].concat(fibsRec(num, i - 1, num2, num2))     
+    }
+    else if(i > 1){
+        let sum = num1 + num2;
+        return [sum].concat(fibsRec(num, i- 1, num2, sum));
+    }
+    else{
+        let sum = num1 + num2;
+        return [sum]
+    }
 }
 
-console.log(fib(8));
+console.log(fibs(8));
+console.log(fibsRec(8));
